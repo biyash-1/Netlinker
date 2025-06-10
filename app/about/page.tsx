bdd5f835a7b64063b18a95fb4f34147e7d817872
biyash-1 <sthaalish39@gmail.com>
2025-06-10 22:55:58 +0545

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,9 @@ const page = () => {
   return (
     <div>
          <h1>Hi ther this is about page</h1>
+         <div>
+          <h1>About developer</h1>
+         </div>
              
     </div>
   )
