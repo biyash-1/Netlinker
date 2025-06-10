2fdf402103e76a869b4d0267ef84751f364fc61b
biyash-1 <sthaalish39@gmail.com>
2025-06-10 22:57:21 +0545

    changess made

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,6 +6,7 @@ const page = () => {
          <h1>Hi ther this is about page</h1>
          <div>
           <h1>About developer</h1>
+          <h1>hi im pasioante software developer from Nepal</h1>
          </div>
              
     </div>
